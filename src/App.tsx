d89811fc9b1f901e8d0f81839b29ecc7af9e7a84
Solovic2 <islammohamed323@gmail.com>
2024-04-15 12:32:29 +0200

    Add Images

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="">Hello world!</h1>
+      <div className="h-screen">Hello world!</div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "mobile-header": "url('assets/mobile/bg-header.svg')",
+        "mobile-sidebar": "url('assets/mobile/bg-sidebar.svg')",
+        "mobile-footer": "url('assets/mobile/bg-footer.svg')",
+        "desktop-header": "url('assets/desktop/bg-header.svg')",
+        "desktop-sidebar": "url('assets/desktop/bg-sidebar.svg')",
+        "desktop-footer": "url('assets/desktop/bg-footer.svg')",
+        "tablet-header": "url('assets/tablet/bg-header.svg')",
+      },
+    },
   },
   plugins: [],
 };
