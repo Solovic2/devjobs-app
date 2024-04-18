c85bb007018defb4e7d9a6970c2b9728f6d10946
Solovic2 <islammohamed323@gmail.com>
2024-04-18 11:47:32 +0200

    Fix Svg Warning

--- src/components/home/CustomCheckBox.tsx
+++ src/components/home/CustomCheckBox.tsx
@@ -13,9 +13,9 @@ const CustomCheckBox = () => {
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <polyline points="20 6 9 17 4 12"></polyline>
       </svg>
