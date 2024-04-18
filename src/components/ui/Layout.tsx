0f7d87dfed528b752832565c83e025e247e42070
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-04-18 19:06:58 +0200

    Fix build issues

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import Navbar from "./Navbar";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return <main className="-mt-10">{children}</main>;
